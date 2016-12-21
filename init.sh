d6e7ea2c6d1799f5d722f99908e2a9cdd7cf0625
Nick Eldridge <nick.eldridge@playmakercrm.com>
2016-12-21 11:54:29 -0500

    Updates to init. I don't remember what they are, I'm a terrible commiterer.

--- init.sh
+++ init.sh
@@ -2,11 +2,13 @@
 yum -y update
 yum -y install httpd24 git
 chkconfig httpd on
-yum install -y php56 php56-cli php56-common php56-devl php56-gd php56-jsonc php56-mbstring php56-mcrypt php56-pdo php56-opcache php56-pecl-imagick php56-xml php56-soap php56-xml php56-xmlrpc php56-mysqlnd
-yum install -y mysql
+#yum install -y php56 php56-cli php56-common php56-devl php56-gd php56-jsonc php56-mbstring php56-mcrypt php56-pdo php56-opcache php56-pecl-imagick php56-xml php56-soap php56-xml php56-xmlrpc php56-mysqlnd
+#yum install -y mysql
+# this whole thing is terribly insecure, running this as root? EleGiggle
 cd /var/www/ && git clone https://github.com/neldridge/neldridge.net.git
 cd /etc/httpd/conf.d/ && ln -s /var/www/neldridge.net/neldridge.net.conf
 rm /etc/httpd/conf.d/welcome.conf
 service httpd start
 echo 'MAILTO=nicholas.eldridge@gmail.com' >> /etc/crontab
+# running this as anyone is probably not ideal, especially root/ec2-user.. too lazy to create an actual user and do proper permissions
 echo '*/5  *  *  *  * root cd /var/www/neldridge.net && /usr/bin/git pull 2>&1 >/dev/null' >> /etc/crontab
