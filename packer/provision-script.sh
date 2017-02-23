46b6d39b97877172c4b47f816fa4fa993b5c82a2
Nick Eldridge <nick.eldridge@playmakercrm.com>
2017-02-23 18:33:51 -0500

    Fixed some logic errors.

--- packer/provision-script.sh
+++ packer/provision-script.sh
@@ -7,23 +7,26 @@ while [[ `/sbin/runlevel | cut -d " " -f 2` != 3 ]]; do
 	sleep 1
 done
 
-# Run level should be good now to do things
-sudo yum -y update
-sudo yum -y install httpd httpd24-tools mod24_ssl
-sudo yum -y install php70 php70-cli php70-common php70-devel php70-json php70-mbstring php70-mcrypt php70-odbc php70-opcache php70-pdo php70-pecl-apcu php70-pecl-apcu-devel php70-pecl-imagick php70-pecl-imagick-devel php70-pecl-oauth php70-pecl-ssh2 php70-pecl-uuid php70-pecl-yaml php70-pgsql php70-xml php70-xmlrpc php70-zip 
+sudo yum -y install httpd24 httpd24-tools mod24_ssl
+#sudo yum -y update
+#sudo yum -y install php70 php70-cli php70-common php70-devel php70-json php70-mbstring php70-mcrypt php70-odbc php70-opcache php70-pdo php70-pecl-apcu php70-pecl-apcu-devel php70-pecl-imagick php70-pecl-imagick-devel php70-pecl-oauth php70-pecl-ssh2 php70-pecl-uuid php70-pecl-yaml php70-pgsql php70-xml php70-xmlrpc php70-zip 
 sudo yum -y install git
 
 sudo chkconfig httpd on
 
-sudo -u apache -- git clone https://github.com/neldridge/neldridge.net.git /var/www/neldridge.net
-sudo ln -s /etc/httpd/conf.d/ /var/www/neldridge.net/neldridge.net.conf
+sudo chown -R apache /var/www
+sudo git clone https://github.com/neldridge/neldridge.net.git /var/www/neldridge.net
+sudo chown -R apache /var/www
+
+if [[ -a "/var/www/neldridge.net/neldridge.net.conf" ]]; then
+	sudo ln -s /var/www/neldridge.net/neldridge.net.conf /etc/httpd/conf.d/
+fi
 sudo rm /etc/httpd/conf.d/welcome.conf
-sudo chown -R apache:apache /var/www
 sudo chmod -R 775 /var/www/neldridge.net
 
 sudo service httpd start
-echo 'MAILTO=nicholas.eldridge@gmail.com' >> /etc/crontab
+echo 'MAILTO=nicholas.eldridge@gmail.com' | sudo tee -a /etc/crontab
 
-echo '*/5  *  *  *  * apache cd /var/www/neldridge.net && /usr/bin/git pull 2>&1 >/dev/null' >> /etc/crontab
+echo '*/5  *  *  *  * apache cd /var/www/neldridge.net && /usr/bin/git pull 2>&1 >/dev/null' | sudo tee -a /etc/crontab
 
 exit 0
